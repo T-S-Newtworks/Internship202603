a78a500ba70ead733c96449b06f03a3df22dfdf2
sagawa-keita <sagawa-keita@tsn.co.jp>
2026-03-23 19:07:54 +0900

    サンプルソース

---
+++ TsnEducationSample/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TsnEducationSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sample()
+        {
+            return View();
+        }
+
+    }
+}
